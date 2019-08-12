ddb4da214efe9429e3ee318588bdcb00710565e3
Tousif Ahmed <tousifkhan510@gmail.com>
2019-08-12 21:23:54 +0530

    updated sql file

--- db-script/db_event_management.sql
+++ db-script/db_event_management.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2016 at 06:35 AM
+-- Generation Time: Aug 11, 2019 at 01:54 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `tbl_holidays` (
 INSERT INTO `tbl_holidays` (`id`, `date`, `reason`, `bdate`) VALUES
 (2, '2016-09-22', 'National day', '2016-08-25 23:21:21'),
 (3, '2016-08-30', 'My sons borthday', '2016-08-26 11:54:44'),
-(4, '2016-12-25', 'Xmas Holiday', '2016-12-20 01:34:45');
+(4, '2018-03-25', 'Xmas Holiday', '2016-12-20 01:34:45');
 
 -- --------------------------------------------------------
 
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `tbl_reservations` (
   `comments` varchar(250) NOT NULL,
   `bdate` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `tbl_reservations`
@@ -91,7 +91,10 @@ INSERT INTO `tbl_reservations` (`id`, `uid`, `ucount`, `rdate`, `status`, `comme
 (2, 10, 3, '2016-08-12 10:10', 'PENDING', '', '2016-08-22 00:53:36'),
 (3, 11, 2, '2016-08-10 20:10', 'APPROVED', '', '2016-08-22 01:09:53'),
 (5, 13, 12, '2016-08-02 10:10', 'PENDING', '', '2016-08-25 18:45:04'),
-(6, 1, 10, '2016-12-20 10:00', 'PENDING', '', '2016-12-20 02:47:04');
+(6, 1, 10, '2016-12-20 10:00', 'PENDING', '', '2016-12-20 02:47:04'),
+(7, 1, 20, '2018-03-12 10:00', 'PENDING', '', '2018-03-08 21:29:00'),
+(8, 11, 20, '2018-03-06 02:00', 'PENDING', '', '2018-03-08 21:29:30'),
+(9, 14, 10, '2018-03-23 05:00', 'PENDING', '', '2018-03-08 21:30:01');
 
 -- --------------------------------------------------------
 
@@ -110,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
   `status` varchar(10) NOT NULL,
   `bdate` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `tbl_users`
@@ -118,11 +121,12 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
 
 INSERT INTO `tbl_users` (`id`, `name`, `pwd`, `address`, `phone`, `email`, `type`, `status`, `bdate`) VALUES
 (1, 'Tousif', '*1D72DAFB953E3606E84902DB8C521C98EC46266E', 'khan here', '11223344', 'tousifkhan510@gmail.com', 'teacher', 'active', '2016-08-21 23:02:38'),
-(10, 'asif', '*C5BE5D8C252812B2CB3D638489B8D8168EA7BAC9', 'my old address', '112234221', 'tousif@gmail.com', 'student', 'active', '2016-08-22 00:53:36'),
+(10, 'Asif', '*1D72DAFB953E3606E84902DB8C521C98EC46266E', 'my old address', '112234221', 'tousif@gmail.com', 'student', 'active', '2016-08-22 00:53:36'),
 (11, 'Ramiz Khan', '*353224D734B504E91E093D721227A863E07D5B83', 'MY Old address', '1122334455', 'ramiz@gmail.com', 'student', 'inactive', '2016-08-22 01:09:53'),
 (13, 'Rizi Khatki', '*353224D734B504E91E093D721227A863E07D5B83', 'some addree here', '2233445566', 'rizi@gmcil.com', 'student', 'active', '2016-08-25 18:45:04'),
-(14, 'ayesha', 'LWCCRM4A', 'adasd asd asd asd a', '23232323', 'aesha@gmail.com', 'teacher', 'active', '2016-12-20 00:36:25'),
-(15, 'admin', '*01A6717B58FF5C7EAFFF6CB7C96F7428EA65FE4C', 'some addresses', '11223344', 'myemail@gmail.com', 'admin', 'active', '2016-12-20 10:00:08');
+(14, 'ayesha', '*1D72DAFB953E3606E84902DB8C521C98EC46266E', 'adasd asd asd asd a', '23232323', 'aesha@gmail.com', 'teacher', 'active', '2016-12-20 00:36:25'),
+(15, 'admin', '*01A6717B58FF5C7EAFFF6CB7C96F7428EA65FE4C', 'some addresses', '11223344', 'myemail@gmail.com', 'admin', 'active', '2016-12-20 10:00:08'),
+(16, 'zeba01', '*1D72DAFB953E3606E84902DB8C521C98EC46266E', 'some address here', '123433342', 'zeba@gmail.com', 'student', 'inactive', '2019-08-11 14:00:02');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
