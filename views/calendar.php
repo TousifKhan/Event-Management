a67afe1465d2534a35496182f3cbd9ef38154ed1
Tousif Ahmed <tousifkhan510@gmail.com>
2019-08-14 18:35:35 +0530

    code clean

--- views/calendar.php
+++ views/calendar.php
@@ -43,7 +43,6 @@ $(function () {
 				},
 				success: function(doc) {
 					var events = [];
-					//console.log('events #########');
 					callback(doc);
 				}
 			});
@@ -52,50 +51,19 @@ $(function () {
         droppable: true, // this allows things to be dropped onto the calendar !!!
         drop: function (date, allDay) { },
 		eventClick: function(calEvent, jsEvent, view) {
-			//alert('Event: ' + calEvent.title);
-			//alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-			//alert('View: ' + view.name);
-			// change the border color just for fun
-			//$(this).css('border-color', 'red');
 		},
 		dayRender: function(date, cell){
-			//console.log('dayRender');
-    		//console.log(date.format());
-			//$(cell).addClass('fc-disabled').html('Disabled');
 			 $(cell).css('opacity', 1);
-			//$(cell).css("display", "none");//    display:none;
 		},
 		viewRender: function(view, element) {},
 		eventRender: function(ev, element, view) {
-			//element.qtip({content: ev.title});
 		},
 		eventAfterRender : function(ev, element, view) {
 			if(ev.block == true) {
-				//console.log('After Render = ' + ev.title + ' BLOCK ? ' + ev.block);
-				//fc-day fc-widget-content
 				var start = ev.start.format();
 				$("td.fc-day[data-date='"+ start +"']").addClass('fc-disabled');
 			}
 		}
-		/*,
-		
-		eventMouseover: function(calEvent, jsEvent) {
-			var tooltip = '<div class="tooltipevent" style="width:100px;height:100px;background:#ccc;position:absolute;z-index:10001;">' + calEvent.title + '</div>';
-			var $tootlip = $(tooltip).appendTo('body');
-		
-			$(this).mouseover(function(e) {
-				$(this).css('z-index', 10000);
-				$tooltip.fadeIn('500');
-				$tooltip.fadeTo('10', 1.9);
-			}).mousemove(function(e) {
-				$tooltip.css('top', e.pageY + 10);
-				$tooltip.css('left', e.pageX + 20);
-			});
-		},
-		eventMouseout: function(calEvent, jsEvent) {
-			$(this).css('z-index', 8);
-			$('.tooltipevent').remove();
-		}*/
 	});
 });//off
 </script>
